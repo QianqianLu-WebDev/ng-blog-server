6580ecffa5ecf5ada9999c9eb05007d4a48c05c4
Amoyensis <amoyensis@outlook.com>
2021-06-01 23:19:53 +0800

    update settings

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
+    project: 'server/tsconfig.json',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -9,7 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
-  root: true,
+  root: false,
   env: {
     node: true,
     jest: true,
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": ["error",{
+      "endOfLine": "auto"}
+    ]
   },
 };
